2bf37b3ace76abdb3eaa1380bb1d77a480a27c41
unknown <temomik44@gmail.com>
2019-11-20 22:45:37 +0300

    ability to save temperatures preset added

--- sketch_oct08a.ino
+++ sketch_oct08a.ino
@@ -5,10 +5,28 @@
 // LiquidCrystal_I2C.h: https://github.com/johnrickman/LiquidCrystal_I2C
 #include <Wire.h> // Library for I2C communication
 #include <LiquidCrystal_I2C.h> // Library for LCD
+#include <EEPROM.h>   
 
 // Wiring: SDA pin is connected to A4 and SCL pin to A5.
 // Connect to LCD via I2C, default address 0x27 (A0-A2 not jumpered)
 LiquidCrystal_I2C lcd = LiquidCrystal_I2C(0x3F, 20, 4); // Change to (0x27,16,2) for 16x2 LCD.
+
+int presetTemp[3];
+void readDataEeprom()
+{
+  for(int i = 0; i < 3; i++)
+  {
+    presetTemp[i] = EEPROM.read(i); 
+  }
+}
+
+void writeDataEeprom()
+{
+  for(int i = 0; i < 3; i++)
+  {
+    EEPROM.update(i,presetTemp[i]); 
+  }
+}
 
 void setup() {
   // Initiate the LCD:
@@ -22,27 +40,38 @@ void setup() {
   pinMode(9,INPUT);
   pinMode(11,INPUT);
   pinMode(12,INPUT);
+  readDataEeprom();
 }
 int PWM = 0;
 int out = 0;
+int presetNum = -1;
 void loop() {
-  
-    
   if(digitalRead(3) == HIGH)
     PWM++;
-    delay(5);
   if(digitalRead(4) == HIGH)
     PWM--;
-  if(digitalRead(5) == HIGH) {
-    PWM = 200;
-  }
   if(digitalRead(9) == HIGH) {
-    PWM = 100;
+    presetNum = 0;
+    PWM = presetTemp[0];
   }
-  if(digitalRead(12) == HIGH)
-    PWM = 222;
+  if(digitalRead(5) == HIGH) {
+    presetNum = 1;
+    PWM = presetTemp[1];
+  }
   if(digitalRead(11) == HIGH)
-    PWM = 300;  
+  { 
+    presetNum = 2;
+    PWM = presetTemp[2];
+  }  
+  if(digitalRead(12) == HIGH)
+  {  
+    if(presetNum >= 0)
+    {
+      presetTemp[presetNum] = PWM;
+    }
+    writeDataEeprom();
+  }
+  
   if(PWM > 400 || PWM < 0 )
     PWM = 0; 
   lcd.setCursor(0, 1);
